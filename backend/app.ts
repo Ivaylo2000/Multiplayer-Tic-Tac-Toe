adab851e4ab02add5b903061700c1b00d91e3892
ivaylo2000 <ivaylo07082000@gmail.com>
2025-10-29 02:32:13 +0200

    test2

--- backend/app.ts
+++ backend/app.ts
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
